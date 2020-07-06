fdd94255fa25f1443876e70bc6f31662ca917f14
Balyerin <balyerin91@gmail.com>
2020-07-06 20:03:44 +0900

    한글테스트2

--- src/main/java/two/spring/test/App.java
+++ src/main/java/two/spring/test/App.java
@@ -10,6 +10,7 @@ public class App
         System.out.println( "Hi my name is not Yerin!!" );
         System.out.println( "Hi my name is not YeongJae!!" );
         System.out.println( "그럼 누구세요? (Hangul test)");
+        System.out.println( "그럼 누구세요? (소스트리 인코딩 설정파일 수정 후 Hangul test2)");
        
     }
 }
