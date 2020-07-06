75d5d1bb51fb562cc3da3dc82a053199a8f065db
Balyerin <balyerin91@gmail.com>
2020-07-06 19:49:57 +0900

    한글테스트

--- src/main/java/two/spring/test/App.java
+++ src/main/java/two/spring/test/App.java
@@ -9,5 +9,7 @@ public class App
     {
         System.out.println( "Hi my name is not Yerin!!" );
         System.out.println( "Hi my name is not YeongJae!!" );
+        System.out.println( "그럼 누구세요? (Hangul test)");
+       
     }
 }
