6adba59baff46a72bc699b023dce9f8c40a1ae21
Yeong <enoughng@naver.com>
2020-07-07 13:32:50 +0900

    ServiceTwoImpl(TransactionTemplate을 이용한 Transaction 관리) ServiceThreeImpl(@Transactional을 이용한 Transaction 관리 추가

---
+++ src/main/java/yeong/spring/store/service/PlaceOrderServiceImplThree.java
@@ -0,0 +1,49 @@
+package yeong.spring.store.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import yeong.spring.store.dao.ItemDAO;
+import yeong.spring.store.dao.PaymentInfoDAO;
+import yeong.spring.store.dao.PurchaseOrderDAO;
+import yeong.spring.store.exception.ItemNotFoundException;
+import yeong.spring.store.vo.Item;
+import yeong.spring.store.vo.PaymentInfo;
+import yeong.spring.store.vo.PurchaseOrder;
+import yeong.spring.store.vo.PurchaseOrderRequest;
+import yeong.spring.store.vo.PurchaseOrderResult;
+
+public class PlaceOrderServiceImplThree implements PlaceOrderService {
+	
+	private ItemDAO itemDAO;
+	private PaymentInfoDAO paymentInfoDAO;
+	private PurchaseOrderDAO purchaseOrderDAO;
+	
+	public void setItemDAO(ItemDAO itemDAO) {
+		this.itemDAO = itemDAO;
+	}
+	
+	public void setPaymentInfoDAO(PaymentInfoDAO paymentInfoDAO) {
+		this.paymentInfoDAO = paymentInfoDAO;
+	}
+	
+	public void setPurchaseOrderDAO(PurchaseOrderDAO purchaseOrderDAO) {
+		this.purchaseOrderDAO = purchaseOrderDAO;
+	}
+
+	@Override
+	@Transactional
+	public PurchaseOrderResult order(PurchaseOrderRequest orderRequest) throws ItemNotFoundException {
+		
+		Item item = itemDAO.findById(orderRequest.getItemId());
+		if(item == null) throw new ItemNotFoundException(orderRequest.getItemId());
+		
+		PaymentInfo paymentInfo = new PaymentInfo(item.getPrice());
+		paymentInfoDAO.insert(paymentInfo);
+		
+		PurchaseOrder order = new PurchaseOrder(item.getId(), orderRequest.getAddress(), paymentInfo.getPrice());
+		purchaseOrderDAO.insert(order);
+		
+		return new PurchaseOrderResult(item, paymentInfo, order);
+	}
+
+}
