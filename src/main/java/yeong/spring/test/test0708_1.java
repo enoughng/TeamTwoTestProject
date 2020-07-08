86153ba8a3b9e1a81395825bfb522bc4e583415e
fmoonblue <fmoonblue@gmail.com>
2020-07-08 09:45:47 +0900

    test0708_1 추가했습니다

---
+++ src/main/java/yeong/spring/test/test0708_1.java
@@ -0,0 +1,10 @@
+package yeong.spring.test;
+
+import java.util.Date;
+
+public class test0708_1 {
+	public static void main(String[] args) {
+		Date now = new Date();
+		System.out.println(now);
+	}
+}
